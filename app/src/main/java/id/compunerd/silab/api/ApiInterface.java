24b1c40b51ea867c04be360d448b0e504773af15
Nenza Nurfirmansyah <nnurfirmansyah@gmail.com>
2018-11-02 20:18:25 +0700

    Penghapusan Activity tidak berguna Perbaiki list dan adapter list ditambahkan data statis dengan array, setiap jenis ada 3 contoh data transaksi belum dibuat

---
+++ app/src/main/java/id/compunerd/silab/api/ApiInterface.java
@@ -0,0 +1,12 @@
+package id.compunerd.silab.api;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+
+public interface ApiInterface {
+
+}
